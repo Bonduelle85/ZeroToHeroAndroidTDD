ddb20593cebda4c6b1cd22582d989a80bde1226a
Anton Gorokhov <bonduelle85@gmail.com>
2024-02-19 22:57:46 +0500

    Task003 Test done, views programmatically

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.LinearLayout.*
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val linearLayout = LinearLayout(this)
+        val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        linearLayout.layoutParams = params
+        linearLayout.orientation = HORIZONTAL
+        linearLayout.gravity = Gravity.CENTER
+
+        val tv = TextView(this)
+        tv.gravity = Gravity.CENTER
+        tv.id = R.id.titleTextView
+        tv.text = getString(R.string.i_am_an_android_developer)
+
+        linearLayout.addView(tv)
+
+        setContentView(linearLayout)
+//        setContentView(R.layout.activity_main)
     }
 }
