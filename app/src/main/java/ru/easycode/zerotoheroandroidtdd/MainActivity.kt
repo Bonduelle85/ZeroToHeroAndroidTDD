83d6166444a35b600b32b10ea28daf27b8cefccf
Anton Gorokhov <bonduelle85@gmail.com>
2024-02-20 22:30:37 +0500

    Task010 Test done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var rootLayout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.removeButton)
+        rootLayout = findViewById(R.id.rootLayout)
+
+        button.setOnClickListener {
+            if (rootLayout.childCount > 1) rootLayout.removeView(textView)
+            button.isEnabled = false
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        rootLayout.removeView(textView)
+        button.isEnabled = false
     }
 }
