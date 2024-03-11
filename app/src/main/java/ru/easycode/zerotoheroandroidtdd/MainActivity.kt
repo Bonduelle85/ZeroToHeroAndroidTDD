1622a2cba4ba7382c0cba3a9768d3d168b013c4c
Anton Gorokhov <bonduelle85@gmail.com>
2024-03-12 00:50:35 +0500

    Task021 Test done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text
+            binding.titleTextView.text = text
+
+            binding.inputEditText.setText("")
+
+            binding.actionButton.isEnabled = false
+        }
+
+        binding.inputEditText.addTextChangedListener { text ->
+            if (text.toString() == "min"){
+                binding.actionButton.isEnabled = true
+            }
+        }
     }
 }
